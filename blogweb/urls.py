ae29b3ec812b4ca611e6911e55088d059763edb6
ajay-makvana <ajaymakvana2001@gmail.com>
2021-10-07 15:32:10 +0530

    Some changes for UI and url direction

--- blogweb/urls.py
+++ blogweb/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',include('home.urls')),
+    path('',include('home.urls')),
     path('blog/',include('blog.urls')),
     path('users/',include('users.urls')),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from blog.models import Post
 
 def index(request):
-    #return HttpResponse('<h1>Okk Google</h1>')
-    return render(request,'home/index.html')
+    most3recentadded = Post.objects.filter(status=1).order_by('-created_on')[:3]
+    most3liked = Post.objects.filter(status=1).order_by('-likes')[:3]
+    context = {'most3recentadded':most3recentadded,'most3liked':most3liked}
+    return render(request,'home/index.html',context)
